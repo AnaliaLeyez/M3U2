c31c9d803bb8a846f44e6419d68b7d2d25101703
Analia Leyez <124401921+AnaliaLeyez@users.noreply.github.com>
2023-04-13 11:40:31 -0700

    configurando form

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var staffRouter = require('./routes/staff'); //busca staff.js
 var bodasRouter = require('./routes/bodas'); //busca staff.js
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,36 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer= require('nodemailer');
+
+router.post('/', async(req, res, next) => {
+  var nombre=req.body.nombre;
+  var email=req.body.email;
+  var asunto=req.body.asunto;
+  var mensaje=req.body.mensaje;
+
+  var obj = {
+    to: 'any15015@gmail.com',
+    subject: 'Contacto Web por fotografia',
+    html: nombre + ' se contactó a través de la web y requiere más información a este correo: ' + email + ". <br> Además dejó un mensaje con el asunto: " + asunto + ". <br> El msj es: " + mensaje
+  }
+
+  var transport =nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+
+  });
+
+  var info= await transport.sendMail(obj);
+
+  res.render('index', {
+    message: 'Mensaje enviado correctamente'
+  });
+
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
