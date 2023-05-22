f8ddfea7f055ccc7b4afbafa7afcdf1392b0e265
Analia Leyez <124401921+AnaliaLeyez@users.noreply.github.com>
2023-05-22 13:16:42 -0700

    doy acceso a sesiones desde la pag principal

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var usersRouter = require('./routes/users');
 var staffRouter = require('./routes/staff'); //busca staff.js
 var bodasRouter = require('./routes/bodas');
 var quinceRouter = require('./routes/quince');
-var sesionRouter = require('./routes/sesion');
+// var sesionRouter = require('./routes/sesion');
 var loginRouter = require('./routes/admin/login');
 var adminRouter= require('./routes/admin/novedades');
 
@@ -52,9 +52,8 @@ app.use('/users', usersRouter);
 app.use('/staff', staffRouter);  //controlador o manejador de ruta
 app.use('/bodas', bodasRouter);
 app.use('/quince', quinceRouter);
-app.use('/sesion', sesionRouter);
+// app.use('/sesion', sesionRouter);
 app.use('/admin/login', loginRouter);
-// app.use('/admin/novedades', adminRouter);
 app.use('/admin/novedades', secured, adminRouter);
 
 app.get('/staff', function (req,res){
@@ -69,27 +68,27 @@ app.get('/quince', function (req,res){
   res.render('quince')
 })
 
-app.get('/sesion', function(req, res) {
-  var conocido= Boolean(req.session.nombre);
+// app.get('/sesion', function(req, res) {
+//   var conocido= Boolean(req.session.nombre);
 
-  res.render('sesion', {
-    titulo: 'Inicio de sesión',
-    conocido: conocido,
-    nombre: req.session.nombre
-  });
-});
+//   res.render('sesion', {
+//     titulo: 'Inicio de sesión',
+//     conocido: conocido,
+//     nombre: req.session.nombre
+//   });
+// });
 
-app.post('/ingresar', function(req, res){
-  if (req.body.nombre) {
-    req.session.nombre= req.body.nombre
-  };
-  res.redirect('/sesion');
-});
+// app.post('/ingresar', function(req, res){
+//   if (req.body.nombre) {
+//     req.session.nombre= req.body.nombre
+//   };
+//   res.redirect('/sesion');
+// });
 
-app.get('/salir', function(req, res){
-  req.session.destroy();
-  res.redirect('/sesion');
-});
+// app.get('/salir', function(req, res){
+//   req.session.destroy();
+//   res.redirect('/sesion');
+// });
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
